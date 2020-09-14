9d69a261bd07f0e29415b8b19c3c97af41482f7d
David Ryan <davidryan0119@gmail.com>
2020-09-14 18:46:12 -0400

    Navbar WIP

--- src/App.js
+++ src/App.js
@@ -1,26 +1,76 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import "./css/App.scss";
+
+// Core Components
+import {
+    AppBar,
+    Toolbar,
+    Typography,
+    Button,
+    IconButton,
+} from "@material-ui/core";
+
+// Icon Components
+import MenuIcon from "@material-ui/icons/Menu";
+import InvertColorsIcon from "@material-ui/icons/InvertColors";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <>
+            <div className="App" style={{ flexgrow: 1 }}>
+                {/* Navigation Bar */}
+                <AppBar color="default">
+                    <Toolbar>
+                        <Button
+                            color="inherit"
+                            style={{ marginright: 8 + "px" }}
+                            href="#"
+                        >
+                            Home
+                        </Button>
+                        <Button
+                            color="inherit"
+                            style={{ marginright: 8 + "px" }}
+                            href="#"
+                        >
+                            About Me
+                        </Button>
+                        <Button
+                            color="inherit"
+                            style={{ marginright: 8 + "px" }}
+                            href="#"
+                        >
+                            Experience
+                        </Button>
+                        <Button
+                            color="inherit"
+                            style={{ marginright: 8 + "px" }}
+                            href="#"
+                        >
+                            Education
+                        </Button>
+                        <Button
+                            color="inherit"
+                            style={{ marginright: 8 + "px" }}
+                            href="#"
+                        >
+                            Portfolio
+                        </Button>
+                        <Button
+                            color="inherit"
+                            style={{ marginright: 8 + "px" }}
+                            href="#"
+                        >
+                            Contact
+                        </Button>
+                        <IconButton aria-label="theme" color="inherit">
+                            <InvertColorsIcon />
+                        </IconButton>
+                    </Toolbar>
+                </AppBar>
+            </div>
+        </>
+    );
 }
 
 export default App;
