2aa2fe14d14ef7c0a1a939976968841cc99028a0
Iswenzz <suxlolz1528@gmail.com>
2020-10-05 20:09:09 +0200

    Navbar: responsive navbar container

--- src/containers/Navbar/Navbar.js
+++ src/containers/Navbar/Navbar.js
@@ -4,40 +4,104 @@ import MenuButtonIcon from "@material-ui/icons/Menu";
 import {
     AppBar,
     Toolbar,
-    Typography,
     Button,
-    IconButton,
+    Menu, withStyles
 } from "@material-ui/core";
-
+import Utility from "../../utility";
+import { useMediaQuery } from "react-responsive";
+import Grid from "@material-ui/core/Grid";
+import IconButton from "@material-ui/core/IconButton";
 import "./Navbar.scss";
-import "../../App.scss"
-import "../../Responsive.scss"
-import Utility from "../../utility"
 
-export default function Navbar()
+export const NavbarMenu = withStyles({
+    paper: {
+        position: "absolute",
+        backgroundColor: "#f5f5f5",
+        width: "100%",
+        padding: 0,
+        margin: 0,
+        left: 0,
+        right: 0
+    },
+})((props) => (
+    <Menu elevation={0} getContentAnchorEl={null} className={"navbar-paper"}
+          anchorOrigin={{vertical: 'bottom', horizontal: 'center'}}
+          transformOrigin={{vertical: 'top', horizontal: 'center'}}
+          {...props}
+    />
+));
+
+/**
+ * @TODO: Remove the ripple effect when the buttons are clicked (especially the menu button)
+ * @returns {JSX.Element}
+ * @constructor
+ */
+export const Navbar = () =>
 {
-    return (
-        <AppBar color="default">
-            <Toolbar>
-                    <Typography className="navbar" type="title" color="inherit" />
-                    {/* 
-                        TODO: Remove the ripple effect when the
-                        buttons are clicked (especially the menu button)
-                    */}
-                    <Button onClick={() => Utility.scrollToElement("home")} hidden>Home</Button>
-                    <Button onClick={() => Utility.scrollToElement("about-me")} hidden>About Me</Button>
-                    <Button onClick={() => Utility.scrollToElement("experience")} hidden>Experience</Button>
-                    <Button onClick={() => Utility.scrollToElement("education")} hidden>Education</Button>
-                    <Button onClick={() => Utility.scrollToElement("portfolio")} hidden>Portfolio</Button>
-                    <Button onClick={() => Utility.scrollToElement("contact")} hidden>Contact</Button>
-                    <IconButton aria-label="theme" color="inherit" hidden>
-                        <InvertColorsIcon />
-                    </IconButton>
+    const isPortrait = useMediaQuery({ orientation: "portrait" });
+    const isTabletOrMobileDevice = useMediaQuery({ query: "(max-device-width: 1224px)" });
+    const [anchorEl, setAnchorEl] = React.useState(null);
 
-                    {/* <IconButton aria-label="theme" color="inherit">
-                        <MenuButtonIcon />
-                    </IconButton> */}
+    const handleClick = (event) =>
+    {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleClose = () =>
+    {
+        setAnchorEl(null);
+    };
+
+    const onLinkClick = (scrollTo) =>
+    {
+        Utility.scrollToElement(scrollTo);
+        handleClose();
+    };
+
+    const links = (
+        <>
+            <Button onClick={() => onLinkClick("home")}>Home</Button>
+            <Button onClick={() => onLinkClick("about-me")}>About Me</Button>
+            <Button onClick={() => onLinkClick("experience")}>Experience</Button>
+            <Button onClick={() => onLinkClick("education")}>Education</Button>
+            <Button onClick={() => onLinkClick("portfolio")}>Portfolio</Button>
+            <Button onClick={() => onLinkClick("contact")}>Contact</Button>
+        </>
+    );
+
+    const mobileNavbar = (
+        <Grid container direction="row" justify="flex-end" alignItems="center">
+            <IconButton onClick={handleClick} aria-label="theme" color="inherit">
+                <MenuButtonIcon />
+            </IconButton>
+            <section>
+                <NavbarMenu id="customized-menu" anchorEl={anchorEl}
+                      keepMounted open={Boolean(anchorEl)}
+                      onClose={handleClose}>
+                    <Grid container direction={"column"} justify={"center"} alignItems={"center"}>
+                        {links}
+                    </Grid>
+                </NavbarMenu>
+            </section>
+        </Grid>
+    );
+
+    const desktopNavbar = (
+        <Grid container direction="row" justify="flex-end" alignItems="center">
+            {links}
+        </Grid>
+    );
+
+    return (
+        <AppBar className="navbar" color="default" component="nav">
+            <Toolbar>
+                {isTabletOrMobileDevice || isPortrait ? mobileNavbar : desktopNavbar}
+                <IconButton aria-label="theme" color="inherit" hidden>
+                    <InvertColorsIcon />
+                </IconButton>
             </Toolbar>
         </AppBar>
     );
-}
+};
+
+export default Navbar;
